fb0c66795bde3600f5ed3a226d62c7b9bfe1feb3
fennecdjay <astor.jeremie@wanadoo.fr>
2016-10-04 23:05:19 +0200

    Yeah. Built from subdir

--- site.sh
+++ site.sh
@@ -7,7 +7,7 @@ function deploy()
 	mv README.md  README.old
 	echo 'built by site/mk_size.sh' > README.md
 	echo '_site' > .gitignore
-	bundle exec jekyll s --config _config.yml,_config_deploy.yml
+	bundle exec jekyll b --config _config.yml,_config_deploy.yml
 	cd _site
 	git add -A
 	git commit -am 'Yeah. Built from subdir'
@@ -24,7 +24,7 @@ function deploy()
 #run the size locally. access with localhost:4000
 function run()
 {
-	bundle exec jekyll s
+	bundle exec jekyll s --config _config.yml
 	rm -rf _site
 }
 
@@ -34,13 +34,22 @@ trap 'rm -rf _site; exit' QUIT
 trap 'rm -rf _site; exit' TERM
 trap 'rm -rf _site; exit' EXIT
 
+function send_gist()
+{
+	echo $(gist -u $1 -f $2| sed "s/https:\/\/gist.github.com\///") >> known_gist
+}
+
 # parse arguments
-if("$1" == "run")
+echo $1
+if [ "${1}" = 'run' ]
 then
-	run 1
-elif("$1" == "deploy")
+	run
+elif [ "${1}" = 'gist' ]
 then
-	./deploy 1
+	send_gist $2 $3
+elif [ "${1}" = 'deploy' ]
+then
+	deploy
 else
 	echo "$0 needs 'run' or 'deploy' as argument"
 fi
